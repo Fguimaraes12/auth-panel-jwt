95d4c6a2a4c1ae1a208170f7b0fe6266b68bbc44
Fguimaraes12 <guimaraesfelipe6021@gmail.com>
2026-09-11 21:30:05 -0300

    fix: stabilize react-query on logout

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,14 +1,16 @@
 'use client'
 
 import postLogin from '@/services/apiServices'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useRouter } from 'next/navigation'
 
 export function useLogin() {
     const router = useRouter()
+    const queryClient = useQueryClient()
     return useMutation({
         mutationFn: postLogin,
         onSuccess: () => {
+            queryClient.removeQueries({ queryKey: ['user'] })
             router.push('/dashboard')
         },
         onError: (error) => {
